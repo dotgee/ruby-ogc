7b4057436b398e2d970300c6f4b9212eeb2aec66
Philippe HUET <philippe@dotgee.fr>
2013-01-24 19:39:21 +0100

    Ajout support GetMap

--- lib/rogc/wms_client.rb
+++ lib/rogc/wms_client.rb
@@ -20,6 +20,10 @@ module ROGC
       @capabilities
     end
 
+    def map(layer, srs = 'CRS:84')
+      build_map_request(layer, srs)
+    end
+
     private
       def build_wms_url(request = 'GetCapabilities', version = "1.3.0", params = {})
         params.merge!({
@@ -31,5 +35,21 @@ module ROGC
         query_string = params.map { |k,v| "#{k}=#{v}" }.join('&')
         "#{@wms_url}?#{query_string}"
       end
+
+      def build_map_request(layer, width = 128, heigth = 128, srs = 'CRS:84', params = {})
+        params.merge!({
+          'format' => 'image/png',
+          'transparent' => true,
+          'layers' => layer.name,
+          'style' => '',
+          'height' => height,
+          'width' => width,
+          'srs'   => srs,
+          'bbox'  => layer.bbox[srs] ? layer.bbox[srs].bbox.join(',') : [11.544243804517265, -0.2895834470186168, 11.802380437565015, -0.0314468139708675].join(',')
+        })
+
+        build_wms_url('GetMap', '1.3.0', params)
+      end
+
   end
 end
